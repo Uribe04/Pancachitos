16cd540b1ce35f9f733df8aa84b5ab6a56f7ce2a
Uribe04 <juanfelipeuribebernal@gmail.com>
2025-10-15 10:10:48 -0500

    Cambios a nav bar

--- Pancachos/src/App.tsx
+++ Pancachos/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/layout/navbar";
 
 function App() {
   return (
-    <div className="bg-gradient-to-b from-blue-300 to-blue-100 min-h-screen flex flex-col items-center p-4">
+    <div>
       <Navbar />    
     </div>
   );
