8e7d4f708c98ba6763eee032262cf79247bf2b00
Sofía Orejuela <sofiaorejuelavelez06@gmail.com>
2025-10-16 15:05:14 -0500

    si

--- Pancachos/src/components/layout/banner.tsx
+++ Pancachos/src/components/layout/banner.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Banner: React.FC = () => {
   return (
-    <section className="relative w-full bg-[#5277FF] flex flex-col items-center overflow-hidden py-12">
-      {/* CONTENIDO PRINCIPAL */}
+    <section className="relative w-full flex flex-col items-center justify-center py-10 bg-gradient-to-r from-[#2971B9] to-[#69ADF1]">
+      
       <div className="flex flex-col items-center justify-center w-full">
-        {/* Imagen desde /public */}
+        
         <img
-          src="/images/Logo banner.png"  // 👈 la imagen debe estar en public/images/
+          src="/images/Logo banner.png"  
           alt="Banner principal"
-          className="w-full max-w-[800px] h-auto object-contain"
+          className="w-full max-w-[700px] h-auto object-contain"
         />
 
         {/* Botón debajo */}
-        <button className="mt-6 bg-[#D1A45F] hover:bg-[#c5974e] text-white font-semibold px-8 py-3 rounded-lg shadow-md transition-all">
+        <button className="mt-1 bg-[#D1A45F] hover:bg-[#c5974e] text-white font-semibold px-8 py-3 rounded-lg shadow-md transition-all">
           SHOW MORE
         </button>
       </div>
